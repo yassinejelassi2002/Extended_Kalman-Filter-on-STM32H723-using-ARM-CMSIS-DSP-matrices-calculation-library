8268323ceb0bf7155b3c4ef1c28d92727eaf4803
Yassine Jelassi <119065478+jelassi23@users.noreply.github.com>
2025-07-21 08:00:28 +0100

    Initial commit

---
+++ Core/Inc/Extended-KF.h
@@ -0,0 +1,62 @@
+#ifndef EKF_H
+#define EKF_H
+#define ARM_MATH_CM7
+#include <arm_math.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+typedef void (*state_func_t)(const arm_matrix_instance_f32* x, const arm_matrix_instance_f32* u, float dt, arm_matrix_instance_f32* x_pred);
+typedef void (*meas_func_t)(const arm_matrix_instance_f32* x, arm_matrix_instance_f32* z_pred);
+typedef void (*jacobian_func_t)(const arm_matrix_instance_f32* x, const arm_matrix_instance_f32* u, float dt, arm_matrix_instance_f32* jacobian);
+
+typedef struct {
+    uint16_t dim_x;
+    uint16_t dim_z;
+
+    arm_matrix_instance_f32 x;      // state vector (dim_x x 1)
+    arm_matrix_instance_f32 P;      // state covariance (dim_x x dim_x)
+    arm_matrix_instance_f32 Q;      // process noise cov (dim_x x dim_x)
+    arm_matrix_instance_f32 R;      // measurement noise cov (dim_z x dim_z)
+
+    // Function pointers for nonlinear models
+    state_func_t f;
+    meas_func_t h;
+    jacobian_func_t F_jacobian;
+    jacobian_func_t H_jacobian;
+
+    // Workspace matrices
+    arm_matrix_instance_f32 Fx;     // Jacobian of f (dim_x x dim_x)
+    arm_matrix_instance_f32 Hx;     // Jacobian of h (dim_z x dim_x)
+    arm_matrix_instance_f32 K;      // Kalman gain (dim_x x dim_z)
+
+    // Buffers for matrix data storage (allocated by user)
+    float* x_data;
+    float* P_data;
+    float* Q_data;
+    float* R_data;
+    float* Fx_data;
+    float* Hx_data;
+    float* K_data;
+} EKF_HandleTypeDef;
+
+void EKF_Init(EKF_HandleTypeDef* ekf,
+              uint16_t dim_x,
+              uint16_t dim_z,
+              float* x_buffer,
+              float* P_buffer,
+              float* Q_buffer,
+              float* R_buffer,
+              float* Fx_buffer,
+              float* Hx_buffer,
+              float* K_buffer,
+              state_func_t f,
+              meas_func_t h,
+              jacobian_func_t F_jacobian,
+              jacobian_func_t H_jacobian);
+
+void EKF_Predict(EKF_HandleTypeDef* ekf, const arm_matrix_instance_f32* u, float dt);
+void EKF_Update(EKF_HandleTypeDef* ekf, const arm_matrix_instance_f32* z);
+void EKF_Step(EKF_HandleTypeDef* ekf, const arm_matrix_instance_f32* u, const arm_matrix_instance_f32* z, float dt);
+
+#endif
---
+++ Core/Src/Extended-KF.c
@@ -0,0 +1,153 @@
+/*
+ * Extended-KF.c
+ *
+ *  Created on: Jul 21, 2025
+ *      Author: yassine jelassi
+ */
+#include "main.h"
+#include "Extended-KF.h"
+#include <string.h>
+
+void EKF_Init(EKF_HandleTypeDef* ekf,
+              uint16_t dim_x,
+              uint16_t dim_z,
+              float* x_buffer,
+              float* P_buffer,
+              float* Q_buffer,
+              float* R_buffer,
+              float* Fx_buffer,
+              float* Hx_buffer,
+              float* K_buffer,
+              state_func_t f,
+              meas_func_t h,
+              jacobian_func_t F_jacobian,
+              jacobian_func_t H_jacobian) {
+    ekf->dim_x = dim_x;
+    ekf->dim_z = dim_z;
+
+    ekf->x_data = x_buffer;
+    ekf->P_data = P_buffer;
+    ekf->Q_data = Q_buffer;
+    ekf->R_data = R_buffer;
+    ekf->Fx_data = Fx_buffer;
+    ekf->Hx_data = Hx_buffer;
+    ekf->K_data = K_buffer;
+
+    arm_mat_init_f32(&ekf->x, dim_x, 1, ekf->x_data);
+    arm_mat_init_f32(&ekf->P, dim_x, dim_x, ekf->P_data);
+    arm_mat_init_f32(&ekf->Q, dim_x, dim_x, ekf->Q_data);
+    arm_mat_init_f32(&ekf->R, dim_z, dim_z, ekf->R_data);
+    arm_mat_init_f32(&ekf->Fx, dim_x, dim_x, ekf->Fx_data);
+    arm_mat_init_f32(&ekf->Hx, dim_z, dim_x, ekf->Hx_data);
+    arm_mat_init_f32(&ekf->K, dim_x, dim_z, ekf->K_data);
+
+    // Initialize function pointers
+    ekf->f = f;
+    ekf->h = h;
+    ekf->F_jacobian = F_jacobian;
+    ekf->H_jacobian = H_jacobian;
+}
+
+void EKF_Predict(EKF_HandleTypeDef* ekf, const arm_matrix_instance_f32* u, float dt) {
+    float x_pred_data[ekf->dim_x];
+    arm_matrix_instance_f32 x_pred;
+    arm_mat_init_f32(&x_pred, ekf->dim_x, 1, x_pred_data);
+
+    ekf->f(&ekf->x, u, dt, &x_pred);
+
+    ekf->F_jacobian(&ekf->x, u, dt, &ekf->Fx);
+
+    // x = f(x,u)
+    memcpy(ekf->x.pData, x_pred.pData, sizeof(float)*ekf->dim_x);
+
+    // P = F P F^T + Q
+    float temp_data[ekf->dim_x * ekf->dim_x];
+    arm_matrix_instance_f32 temp, FxT;
+    arm_mat_init_f32(&temp, ekf->dim_x, ekf->dim_x, temp_data);
+    arm_mat_init_f32(&FxT, ekf->dim_x, ekf->dim_x, temp_data); // reuse buffer for transpose
+
+    arm_mat_mult_f32(&ekf->Fx, &ekf->P, &temp);
+    arm_mat_trans_f32(&ekf->Fx, &FxT);
+    arm_mat_mult_f32(&temp, &FxT, &ekf->P);
+    arm_mat_add_f32(&ekf->P, &ekf->Q, &ekf->P);
+}
+
+void EKF_Update(EKF_HandleTypeDef* ekf, const arm_matrix_instance_f32* z) {
+    ekf->H_jacobian(&ekf->x, NULL, 0, &ekf->Hx);
+
+    float z_pred_data[ekf->dim_z];
+    arm_matrix_instance_f32 z_pred;
+    arm_mat_init_f32(&z_pred, ekf->dim_z, 1, z_pred_data);
+
+    ekf->h(&ekf->x, &z_pred);
+
+    // y = z - h(x)
+    float y_data[ekf->dim_z];
+    arm_matrix_instance_f32 y;
+    arm_mat_init_f32(&y, ekf->dim_z, 1, y_data);
+    arm_mat_sub_f32(z, &z_pred, &y);
+
+    // S = H P H^T + R
+    float HP_data[ekf->dim_z * ekf->dim_x];
+    float HxT_data[ekf->dim_x * ekf->dim_z];
+    float S_data[ekf->dim_z * ekf->dim_z];
+    arm_matrix_instance_f32 HP, HxT, S;
+    arm_mat_init_f32(&HP, ekf->dim_z, ekf->dim_x, HP_data);
+    arm_mat_init_f32(&HxT, ekf->dim_x, ekf->dim_z, HxT_data);
+    arm_mat_init_f32(&S, ekf->dim_z, ekf->dim_z, S_data);
+
+    arm_mat_mult_f32(&ekf->Hx, &ekf->P, &HP);
+    arm_mat_trans_f32(&ekf->Hx, &HxT);
+    arm_mat_mult_f32(&HP, &HxT, &S);
+    arm_mat_add_f32(&S, &ekf->R, &S);
+
+    // Compute inverse of S
+    arm_matrix_instance_f32 S_inv;
+    float S_inv_data[ekf->dim_z * ekf->dim_z];
+    arm_mat_init_f32(&S_inv, ekf->dim_z, ekf->dim_z, S_inv_data);
+    if (arm_mat_inverse_f32(&S, &S_inv) != ARM_MATH_SUCCESS) {
+        // Handle inversion failure (singular matrix)
+        return;
+    }
+
+    // K = P H^T S^{-1}
+    float PHt_data[ekf->dim_x * ekf->dim_z];
+    arm_matrix_instance_f32 PHt;
+    arm_mat_init_f32(&PHt, ekf->dim_x, ekf->dim_z, PHt_data);
+
+    arm_mat_mult_f32(&ekf->P, &HxT, &PHt);
+    arm_mat_mult_f32(&PHt, &S_inv, &ekf->K);
+
+    // x = x + K y
+    float Ky_data[ekf->dim_x];
+    arm_matrix_instance_f32 Ky;
+    arm_mat_init_f32(&Ky, ekf->dim_x, 1, Ky_data);
+    arm_mat_mult_f32(&ekf->K, &y, &Ky);
+    arm_mat_add_f32(&ekf->x, &Ky, &ekf->x);
+
+    // P = (I - K H) P
+    float KH_data[ekf->dim_x * ekf->dim_x];
+    arm_matrix_instance_f32 KH;
+    arm_mat_init_f32(&KH, ekf->dim_x, ekf->dim_x, KH_data);
+    arm_mat_mult_f32(&ekf->K, &ekf->Hx, &KH);
+
+    float I_data[ekf->dim_x * ekf->dim_x];
+    arm_matrix_instance_f32 I, I_minus_KH;
+    arm_mat_init_f32(&I, ekf->dim_x, ekf->dim_x, I_data);
+    arm_mat_init_f32(&I_minus_KH, ekf->dim_x, ekf->dim_x, I_data); // reuse I_data buffer
+
+    // Create identity matrix
+    for (uint16_t i = 0; i < ekf->dim_x * ekf->dim_x; i++) I_data[i] = 0.0f;
+    for (uint16_t i = 0; i < ekf->dim_x; i++) I_data[i * ekf->dim_x + i] = 1.0f;
+
+    arm_mat_sub_f32(&I, &KH, &I_minus_KH);
+    arm_mat_mult_f32(&I_minus_KH, &ekf->P, &ekf->P);
+}
+
+void EKF_Step(EKF_HandleTypeDef* ekf, const arm_matrix_instance_f32* u, const arm_matrix_instance_f32* z, float dt) {
+    EKF_Predict(ekf, u, dt);
+    EKF_Update(ekf, z);
+
+}
+
+
